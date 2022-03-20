5c769669006c4e8786c19178d1c7f64d81280a1e
Wil Johnson <15970051+wilj@users.noreply.github.com>
2022-03-20 11:52:18 -0400

    added more memory logging output as csv

--- watch_memory.sh
+++ watch_memory.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-while true; do
-    echo "$(date '+%Y-%m-%d %H:%M:%S') $(free -m | grep Mem: | sed 's/Mem://g' | awk '{ print $NF }' )"
-    sleep 1
-done
+watchMemory() {
+  echo "log_ts,pct_mem,vsz,rss"
+  while true; do
+      echo "$(date '+%Y-%m-%d %H:%M:%S'),$(ps -C dolt  -o%mem=,vsz=,rss= | sed 's/^\s//g' | sed 's/\s/,/g')" 
+      sleep 1
+  done
+}
+
+watchMemory | tee memorylog.csv
